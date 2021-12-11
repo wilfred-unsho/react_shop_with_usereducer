76ec0e916c3caa3737d767a2c78422be30083fdb
Wilfred <wilf.funsho@gmail.com>
2021-12-11 16:52:24 +0100

    React Shop App - Complex State Management, useReducer, context

---
+++ src/services/useFetch.jsx
@@ -0,0 +1,40 @@
+import React, {useState, useEffect, useRef} from 'react';
+
+const baseUrl = process.env.REACT_APP_API_BASE_URL;
+
+export default function useFetch(url) {
+    // Declare state variable
+    const isMounted = useRef(false)
+    const [data, setData] = useState(null)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+
+    useEffect(() => {
+        isMounted.current = true
+        async function init(){
+            try{
+                const response = await fetch(baseUrl + url)
+                if(response.ok)
+                {
+                    const json = await response.json()
+                    if(isMounted.current) setData(json)
+                }else{
+                    throw error
+                }
+            }catch (e) {
+                if(isMounted.current) setError(e)
+            }finally {
+                if(isMounted.current) setLoading(false)
+            }
+        }
+
+        init()
+
+        return () => {
+            isMounted.current = false
+        }
+
+    }, [url]);
+    return {data, error, loading}
+}
